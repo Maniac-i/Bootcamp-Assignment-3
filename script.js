b9024848d77bf0ac06125f6fa74b020db053aebb
Joe Maniaci <joe.maniaci1@gmail.com>
2020-10-25 14:13:28 -0400

    created function characterSet that determines the character set used for the password generator

--- script.js
+++ script.js
@@ -13,33 +13,58 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//User prompts for password criteria
+passwordLength = prompt("Enter a password lenght between 8 and 128");
+passwordLowerCase = confirm("Do you want to use lowercase letters");
+passwordUpperCase = confirm("Do you want to use uppercase letters");
+passwordNumbers = confirm("Do you want to use numbers");
+passwordSpecial = confirm("Do you want to use special characters");
+
 //Variables for character sets
 var passwordString = "";
 var specialCharacters = "`~!@$%^&*()_-[]{}|?.,<>+=;:";
 var lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
 var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789"
-
+var charSet = "";
 
 //Picks random character from character set and adds it to the passwordString
 function pickRandom() {
   passwordString += charSet.charAt(Math.floor(Math.random() * charSet.length));
 }
 
+//Determines character set for the password
+function characaterSet() {
+
+  if (passwordLowerCase === true){
+    charSet += lowerAlpha;
+  } if (passwordUpperCase === true) {
+    charSet += upperAlpha;
+  } if (passwordNumbers === true) {
+    charSet += numbers;
+  } if (passwordSpecial === true) {
+    charSet += specialCharacters;
+  }
+
+}
+
+
 //Generates password
 function generatePassword() {
 
-  passwordLength = prompt("Enter a password lenght between 8 and 128");
-  passwordLowerCase = confirm("Do you want to use lowercase letters");
-  passwordUpperCase = confirm("Do you want to use uppercase letters");
-  passwordNumbers = confirm("Do you want to use numbers");
-  passwordSpecial = confirm("Do you want to use special characters");
 
 for (var i = 0; i < passwordLength; i++) {
 
-  if (passwordLowerCase === true && passwordUpperCase === true && passwordNumbers === true && passwordSpecial === true);
-    charSet = numbers + specialCharacters + lowerAlpha + upperAlpha;
+  characaterSet()
+    
+  if (passwordLowerCase === true && passwordUpperCase === true && passwordNumbers === true && passwordSpecial === true) {
     pickRandom();
+  } else if (passwordLowerCase === true && passwordUpperCase === true && passwordNumbers === true && passwordSpecial === false) {
+    pickRandom();
+  } else if (passwordLowerCase === true && passwordUpperCase === true && passwordNumbers === false && passwordSpecial === true) {
+    pickRandom();
+  } 
+
   }
-return passwordString;
+  return passwordString;
 }
